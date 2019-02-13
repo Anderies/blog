a96d34fd1932299035565bd787205179a642589f
andreas <ndreznotanto@gmail.com>
2019-02-13 23:39:40 +0700

    templating part 2

--- application/views/detail.php
+++ application/views/detail.php
@@ -1,3 +1,34 @@
-<h1><?php echo $blog['title']; ?></h1>
+<?php $this->load->view('partial/header');?>
+<!-- Page Header -->
+  <header class="masthead" style="background-image: url('<?php echo base_url();?>assets/img/post-bg.jpg')">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="post-heading">
+            <h1><?php echo $blog['title']; ?></h1>
+            <h2 class="subheading">Problems look mighty small from 150 miles up</h2>
+            <span class="meta">Posted on <?php echo $blog['date']?></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
 
-<?php echo $blog['content'];?>
+  <!-- Post Content -->
+  <article>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+        	<?php echo $blog['content'];?>
+        </div>
+      </div>
+    </div>
+  </article>
+
+  <hr>
+
+
+
+
+<?php $this->load->view('partial/footer');?>	
